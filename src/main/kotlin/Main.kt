55a48386c4014cb900bd83eeeb1217d40ca33857
Дубинин Родион Александрович <2078-23@std.pkgh.edu>
2025-09-06 15:46:32 +0300

    DubininKorneeva_project1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,41 @@
 fun main() {
-    val testt = ("AAADSSSRRTTHAAAA")
-    val res = CharArray(testt.length)
-    var  k: Int = 1
-    var j: Int = 0
+    val com: Int = readln().toInt()
 
 
-    for(i in 0 until  testt.length-1)
-    {
-        if (testt[i] == testt[i+1]) {
-            k++;
-        }
+    when (com) {
+        1 -> {
+            val testt = ("AAADSSSRRTTHAAAA")
+            val res = CharArray(testt.length)
+            var k: Int = 1
+            var j: Int = 0
 
-        else
-        {
-            res[j]= testt[i]
+
+            for (i in 0 until testt.length - 1) {
+                if (testt[i] == testt[i + 1]) {
+                    k++;
+                } else {
+                    res[j] = testt[i]
+                    j++
+
+                    if (k > 1) {
+                        res[j] = (k + '0'.code).toChar()
+                        j++
+                    }
+                    k = 1
+
+                }
+            }
+            res[j] = testt.last()
             j++
-
             if (k > 1) {
                 res[j] = (k + '0'.code).toChar()
                 j++
             }
-            k=1
+
+            res.forEach { print(it.toString() + "\t") }
+        }
+        2->{
 
         }
     }
-    res[j] = testt.last()
-    j++
-    if (k > 1) {
-        res[j] = (k + '0'.code).toChar()
-        j++
-    }
-
-    res.forEach { print(it.toString()+"\t") }
-
 }
