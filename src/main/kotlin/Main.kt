ce607980fe4014ea71cd489d8cf5452546f46406
Дубинин Родион Александрович <2078-23@std.pkgh.edu>
2025-09-06 15:36:01 +0300

    DubininKorneeva_project1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val testt = ("AAADSSSRRTTHAAAA")
+    val res = CharArray(testt.length)
+    var  k: Int = 1
+    var j: Int = 0
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    for(i in 0 until  testt.length-1)
+    {
+        if (testt[i] == testt[i+1]) {
+            k++;
+        }
+
+        else
+        {
+            res[j]= testt[i]
+            j++
+
+            if (k > 1) {
+                res[j] = (k + '0'.code).toChar()
+                j++
+            }
+            k=1
+
+        }
+    }
+    res[j] = testt.last()
+    j++
+    if (k > 1) {
+        res[j] = (k + '0'.code).toChar()
+        j++
+    }
+
+    res.forEach { print(it.toString()+"\t") }
+
 }
